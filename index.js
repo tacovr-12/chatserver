56c47d3921465afa3240bbbad4b7b8f0379f4e79
tacovr-12 <thetacoman12@outlook.com>
2025-11-24 19:19:58 -0600

    update

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ import { Server } from "socket.io";
 import path from "path";
 import { fileURLToPath } from "url";
 import fs from "fs";
-import fetch from "node-fetch"; // npm install node-fetch
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
